72b260e239882c0485867cefb7df8b2e2b9bd882
Clement <clementnyankuadjei@gmail.com>
2025-07-08 18:03:26 +0000

    Refactor confessions service: move to core directory and remove unused test and service files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { ConfessionsService, Confession } from './services/confessions.service';
+import { ConfessionsService, Confession } from './core/services/confessions.service';
 
 @Component({
   selector: 'app-root',
